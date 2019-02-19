b522036867064ff494dfd491075ed1193c66b39e
jason-derry <jason.derry@academytrainee.com>
2019-02-19 11:56:31 +0000

    removed hard-coded string

--- src/main/java/com/qa/player/service/PlayerServiceImpl.java
+++ src/main/java/com/qa/player/service/PlayerServiceImpl.java
@@ -2,6 +2,7 @@ package com.qa.player.service;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -11,18 +12,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class PlayerServiceImpl implements PlayerService {
 	
 	ObjectMapper mapper = new ObjectMapper();
+	@Value("${choice.attribute}")
+	private String attribute;
 
 	public Boolean setPlaying(String player) {
 		JsonNode realPlayer;
 		try {
 			realPlayer = mapper.readTree(player);
-			if (realPlayer.get("playing").asBoolean() == true) {
+			if (realPlayer.get(attribute).asBoolean() == true) {
 				return false;
 			} else {
 				return true;
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return false;
 		}
