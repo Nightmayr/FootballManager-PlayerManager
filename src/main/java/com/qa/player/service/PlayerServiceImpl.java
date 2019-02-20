b8d733632e405490a36e5d03d085785fb815f335
jason-derry <jason.derry@academytrainee.com>
2019-02-20 12:04:58 +0000

    restructured code

--- src/main/java/com/qa/player/service/PlayerServiceImpl.java
+++ src/main/java/com/qa/player/service/PlayerServiceImpl.java
@@ -6,13 +6,7 @@ import org.springframework.stereotype.Service;
 public class PlayerServiceImpl implements PlayerService {
 	
 	public Boolean setPlaying(String isPlaying) {
-		boolean playing = false;
-		if ("false".equalsIgnoreCase(isPlaying)) {
-			playing = true;
-		} else {
-			playing = false;
-		} 
-		return playing;
+		return ("false".equalsIgnoreCase(isPlaying)) ? true : false;
 	} 
 
 }
