3f5cc58c88b8aff7a3cccf697efed38f405b0161
jason-derry <jason.derry@academytrainee.com>
2019-02-20 11:34:11 +0000

    empty constructor removed

--- src/main/java/com/qa/player/PlayerApplication.java
+++ src/main/java/com/qa/player/PlayerApplication.java
@@ -5,10 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlayerApplication {
-	
-	private PlayerApplication() {
-		//Empty constructor
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlayerApplication.class, args);
